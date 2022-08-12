d88528d05ffe439dc536c923966c9e33abcdb837
Severino Thomasin <s.thomasin@gmail.com>
2022-08-12 20:29:17 +0200

    added selinas color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,10 @@ module.exports = {
     extend: {
       colors: {
         sel: {
-          '100': '#dfcfb6'
+          '100': '#dfcfb6',
+          '200': '#e4afa8',
+          '300': '#c7762e',
+          '400': '#567c68',
         }
       }
     },
