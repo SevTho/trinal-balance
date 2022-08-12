c8ffc4d50ddede33ae6707bf4828745ed449de25
Severino Thomasin <s.thomasin@gmail.com>
2022-08-12 18:44:18 +0200

    project setup with tailwind support

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { TailwindProvider } from "tailwindcss-react-native";
 
 export default function App() {
