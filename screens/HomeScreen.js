7c09098b68394382f924e43a75182d9b59510a17
Severino Thomasin <s.thomasin@gmail.com>
2022-08-13 12:49:13 +0200

    add icon to daily panel

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { Text, View } from 'react-native'
+import { Text, View, Image } from 'react-native'
 import React from 'react'
 
 const HomeScreen = () => {
@@ -7,12 +7,23 @@ const HomeScreen = () => {
     
       { /* Titlebar */}
 
-      <View>
+      <View className='mt-5 flex flex-row space-x-5'>
         <View>
-          <Text className='uppercase text-xl'>Guten Morgen Annika!</Text>
-          <Text>Du befindest dich heute in der</Text>
-          <Text>Lutealphase (15. Zyklustag)</Text>
+          <Text className='uppercase text-xl text-slate-800'>Guten Morgen Annika!</Text>
+          <Text className='text-slate-800'>Du befindest dich heute in der</Text>
+          <Text className='text-slate-800'>Lutealphase (15. Zyklustag)</Text>
         </View>
+        <Image 
+          source={{
+            uri: 'https://pps.whatsapp.net/v/t61.24694-24/294946175_727290145195026_6571170971301202090_n.jpg?ccb=11-4&oh=01_AVypQEqYUCfNYfdtM9S-Bd-McJEHFHZbnkZDzvAR-4UTjg&oe=6304CC1C',
+          }}
+          className='h-14 w-14 bg-gray-300 rounded-full mt-3'
+        />
+      </View>
+
+      {/* First Card */}
+      <View>
+
       </View>
 
     </View>
